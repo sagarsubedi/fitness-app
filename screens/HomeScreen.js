6c3a835d30db65f40b9f807e19f0848a656065d9
Sagar Subedi <sagar@example.com>
2022-10-30 16:06:45 -0500

    user can logout and update password

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,86 +1,34 @@
-import { SafeAreaView,  StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import { auth } from '../firebase'
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import ProfileScreen from './ProfileScreen';
+import CalculateScreen from './CalculateScreen';
+import Ionicons from 'react-native-vector-icons/Ionicons'
 
+const Tab = createBottomTabNavigator();
 
 const HomeScreen = ({navigation}) => {
 
-  const handleLogOut = () => {
-    auth.signOut()
-      .then(() => {
-        console.log('User signed out')
-      navigation.replace('Login')
-    })
-  }
-
   return (
-    <SafeAreaView>
-      <View style={styles.buttonContainer}>
-        <Text className=''>Email: {auth.currentUser?.email}</Text>
-        <TouchableOpacity
-          onPress={handleLogOut}
-          style={styles.button}
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          if (route.name === 'Calculate') {
+            iconName = 'calculator'
+          } else if (route.name === 'Profile') {
+            iconName = 'pencil'
 
-        >
-          <Text style={styles.buttonText}>Sign Out</Text>
-        </TouchableOpacity>     
-      </View>
-    </SafeAreaView>
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: '#4DBA8D',
+        tabBarInactiveTintColor: 'gray',
+      })}
+    >
+      <Tab.Screen name="Calculate" component={CalculateScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
   )
 }
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  inputContainer: {
-    width: '80%',
-  },
-  
-  input: {
-    backgroundColor: 'white',
-    paddingHorizontal: 15,
-    paddingVertical: 10,
-    borderRadius: 10,
-    marginTop: 5,
-  },
-
-  buttonContainer: {
-    width: '60%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 40,
-  },
-
-  button: {
-    backgroundColor: '#4DBA8D',
-    width: '100%',
-    padding:15,
-    borderRadius: 10,
-    alignItems: 'center',
-  },
-
-  buttonOutline: {
-    backgroundColor: 'white',
-    margin: 5,
-    borderColor: '#4DBA8D',
-    borderWidth: 2,
-  },
-
-  buttonText: {
-    color: 'white',
-    fontWeight: '700',
-    fontSize: 16,
-  },
-
-  buttonOutlineText: {
-    color: '#4DBA8D',
-    fontWeight: '700',
-    fontSize: 16,
-  },
-})
 export default HomeScreen
---
+++ screens/ProfileScreen.js
@@ -0,0 +1,136 @@
+import { StyleSheet, Text, View, Button,KeyboardAvoidingView, TextInput, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import { auth } from '../firebase'
+
+
+const ProfileScreen = ({ navigation }) => {
+  
+  const [newPassword, setNewPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+
+  const handleLogOut = () => {
+    auth.signOut()
+      .then(() => {
+        console.log('User signed out')
+      navigation.replace('Login')
+      })
+      .catch((error) => alert(error.message))
+    
+  }
+
+  const handleUpdatePassword = () => {
+    if (newPassword !== confirmPassword) {
+      alert('Passwords do not match')
+      // reset the state and clear the input fields
+      setNewPassword('')
+      setConfirmPassword('')
+      return
+    }
+    auth.currentUser.updatePassword(newPassword)
+      .then(() => {
+        alert('Password successfully updated')
+      })
+      .catch((error) => alert('Something went wrong. Please try again.'))
+   }
+  
+  return (
+    <View className='h-full flex justify-between items-center'>
+
+      {/* update password */}
+      <View className='mt-4 w-full'>
+        <Text className='ml-2 text-secondary font-bold'>Change Password</Text>
+        <View className='border-b-4 border-indigo-500 mt-2' />
+        <View className='mt-4 flex justify-center items-center'>
+          <KeyboardAvoidingView style={styles.inputContainer}>
+            <TextInput
+              value={newPassword}
+              onChangeText={(newPassword) => setNewPassword(newPassword)}
+              placeholder='New Password'
+              keyboardType='text'
+              secureTextEntry
+              style={styles.input}
+            />
+
+            <TextInput
+              value={confirmPassword}
+              onChangeText={(confirmPassword) => setConfirmPassword(confirmPassword)}
+              placeholder='Confirm Password'
+              keyboardType='text'
+              secureTextEntry
+              style={styles.input}
+            />
+          </KeyboardAvoidingView>
+          <View className='mt-4'>
+            {/* <Button title='Update' onPress={handleUpdatePassword} style={styles.button} /> */}
+            <TouchableOpacity
+              onPress={handleUpdatePassword}
+              style={styles.button}
+
+            >
+              <Text style={styles.buttonText}>Update</Text>
+            </TouchableOpacity>
+          </View>
+
+        </View>
+      </View>
+
+
+
+      <View className='mt-14'>
+        <Text>Change Email</Text>
+      </View>
+
+      <View className='mb-2'>
+        <TouchableOpacity
+          onPress={handleLogOut}
+          style={[styles.button, styles.buttonOutline]}
+        >
+          <Text style={styles.buttonOutlineText}>Log Out</Text>
+        </TouchableOpacity>
+      </View> 
+    </View>
+  )
+}
+
+export default ProfileScreen
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    width: '80%',
+  },
+  
+  input: {
+    backgroundColor: 'white',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 20,
+  },
+
+  button: {
+    backgroundColor: '#4DBA8D',
+    width: '100%',
+    padding:15,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+
+  buttonText: {
+    color: 'white',
+    fontWeight: '700',
+    fontSize: 16,
+  },
+  
+  buttonOutline: {
+    backgroundColor: 'white',
+    margin: 5,
+    borderColor: '#4DBA8D',
+    borderWidth: 2,
+  },
+
+  buttonOutlineText: {
+    color: '#4DBA8D',
+    fontWeight: '700',
+    fontSize: 16,
+  },
+})
